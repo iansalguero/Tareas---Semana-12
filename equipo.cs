1800af4311ae2691a537774566a86dfbca688237
iansalguero <114033098+iansalguero@users.noreply.github.com>
2022-11-07 09:43:38 -0600

    Add files via upload

---
+++ equipo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T12_IPSW_1151222
+{
+    internal class equipo
+    {
+        public string nombre;
+    }
+}
